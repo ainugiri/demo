7a96e0d631874846cb404ca35b63aadf690969d5
ainugiri12 <ainugiri@gmail.com>
2023-04-26 16:48:25 +0530

    Change_02_Updaed

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.logging.Logger;
+
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+	
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
