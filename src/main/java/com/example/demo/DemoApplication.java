f13a48b5c32fe94037278fdf40f63f1535a85265
ainugiri12 <ainugiri@gmail.com>
2023-04-26 16:37:57 +0530

    Changes01_Updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,25 +1,12 @@
 package com.example.demo;
 
-import java.util.logging.Logger;
-
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 public class DemoApplication {
-	public static Logger loginfo = (Logger) LoggerFactory.getLogger(DemoApplication.class);
 	
-	@PostConstruct
-	public void initialize()
-	{
-		loginfo.info("Application start");
-	}
 	public static void main(String[] args) {
-		loginfo.info("Main start");
-		loginfo.info(" start");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
